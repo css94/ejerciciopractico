5733b4f72bc429cf3a819d10ba6ca5fde4a29ce7
css94 <carlotasanchezsansegundo@gmail.com>
2017-02-24 17:38:14 +0100

    commit

---
+++ src/paquete22/InicioXML.java
@@ -0,0 +1,22 @@
+package paquete22;
+
+import java.io.FileInputStream;
+import java.util.List;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+public class InicioXML {
+	
+	public static void main(String[] args) throws Throwable {
+		
+		XMLReader reader = XMLReaderFactory.createXMLReader();
+		ParserLibroXML parserlibro = new ParserLibroXML();
+		
+		reader.setContentHandler(parserlibro);
+		
+		reader.parse(new InputSource(new FileInputStream("libros.xml")));
+		
+	}
+}
