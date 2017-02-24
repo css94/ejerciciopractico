d1e50a6c4d9af2d4086ee5e3d21c96d779f58252
css94 <carlotasanchezsansegundo@gmail.com>
2017-02-24 18:01:59 +0100

    commit

--- src/paquete22/MainLibro.java
+++ src/paquete22/MainLibro.java
@@ -7,7 +7,7 @@ import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-public class InicioXML {
+public class MainLibro {
 	
 	public static void main(String[] args) throws Throwable {
 		
